9647e5971aead79601f0ae4f79d004bd6e238929
GeorgeSpinner <98502105+GeorgeSpinner@users.noreply.github.com>
2022-01-31 01:45:09 -0500

    Added sleep mode after execution

--- adafruit_trinket_PICKnMIX.ino
+++ adafruit_trinket_PICKnMIX.ino
@@ -10,23 +10,30 @@
 //  0   - JAMMA PIN 17
 //  1   - JAMMA PIN 25
 //
-// In the above setup - The Holding the D button during POWER ON will skip PICKnMIX
+// In the above setup - holding the D button during POWER ON will skip PICKnMIX and display the 161 in 1 menu
 //
 // Does NOT work with standard Adafruit Trinket Mini 3v/5v bootloader (takes more than 2 seconds before code executes)
 // Must use Bootloader found here: https://forums.adafruit.com/download/file.php?id=34218 (trinketloader_skippow25.zip) 
 
-int button;                  // to track D button status (HIGH or LOW)
+#include <avr/sleep.h>
 
+// define pin settings
+
+int START = 0;                            // Start button connected to this pin (JAMMA pin 17) 
+int PNM_DISABLE = 1;                      // button that will disable PICKnMIX connected to this pin (JAMMA pin 22=A 23=B 24=C 25=D)
 void setup() {
-  pinMode(0, OUTPUT);        // sets the digital pin 0 as OUTPUT
-  pinMode(1, INPUT);         // sets the digital pin 1 ( D button)
-  button = digitalRead(1);   // read the input pin (is D button pressed - in LOW state)
-  if (button == HIGH) {      // If D button is NOT pressed (still in HIGH state)
-    digitalWrite(0, LOW);    // sets the digital pin 0 to GND (Hold Start)
-    delay(5000);             // waits for 5 seconds
+  sleep_disable();                        // disable sleep mode 
+  pinMode(START, OUTPUT);                 // sets the digital pin 0 connected to Start button as OUTPUT
+  pinMode(PNM_DISABLE, INPUT);            // sets the digital pin 1 connected to PNM_DISABLE button as INPUT
+  if (digitalRead(PNM_DISABLE) == HIGH) { // If PNM_DISABLE button is NOT pressed (still in HIGH state)
+    digitalWrite(START, LOW);             // sets the digital pin 0 to GND (Hold Start button)
+    delay(5000);                          // waits for 5 seconds to enable PICKnMIX
   }
-  pinMode(0, INPUT);         // sets the digital pin 0 as INPUT (Release Start) 
+  pinMode(START, INPUT);                  // sets the digital pin 0 as INPUT (Release Start button) 
 }
 
 void loop() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // set sleep mode
+  sleep_enable();                         // enable sleep
+  sleep_cpu();                            // put the cpu to sleep
 }
